2e19c44ff8ea35263cdd287b829779e44cc18a63
Tor-YA <69291928+Tor-YA@users.noreply.github.com>
2023-07-19 15:28:51 +0300

    Added Page Object

---
+++ src/test/java/pages/DashboardPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import baseEntities.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DashboardPage extends BasePage {
+    private final static String pagePath = "/index.php?/dashboard/";
+
+    private final By headerTitleLabelLocator = By.xpath("//div[contains(@class, 'content-header-title') and contains(text(), 'All Projects')]");
+
+    public TopMenuPage topMenuPage;
+    public SideMenuPage sideMenuPage;
+
+    public DashboardPage(WebDriver driver) {
+        super(driver);
+
+        topMenuPage = new TopMenuPage(driver);
+        sideMenuPage = new SideMenuPage(driver);
+    }
+
+    @Override
+    protected By getPageIdentifier() {
+        return headerTitleLabelLocator;
+    }
+
+    public void openPageByUrl() {
+        super.openPageByUrl(pagePath);
+    }
+
+    public WebElement getHeaderTitle() {
+        return driver.findElement(headerTitleLabelLocator);
+    }
+}
